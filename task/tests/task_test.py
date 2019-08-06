a5213cd9c21c8dfe8efca65d673f438dcd8035fe
Maiorovaek <katushkm@yandex.ru>
2019-08-06 23:44:30 +0300

    Adding a test

--- task/tests/task_test.py
+++ task/tests/task_test.py
@@ -29,3 +29,17 @@ def test_room_paths():
     center.add_paths({"north": north, "south": south})
     assert_equal(center.go('north'), north)
     assert_equal(center.go('south'), south)
+
+
+def test_map():
+    start = Room("Start", "You can go west and down a hole.")
+    west = Room("Trees", "Therer are trees here, you can go east")
+    down = Room("Dungeon", "it's dark down here, you can go up")
+    start.add_paths({"west":west, "down":down})
+    west.add_paths({"east": start})
+    down.add_paths({"up": start})
+
+    assert_equal(start.go('west'), west)
+    assert_equal(start.go('west').go('east'), start)
+    assert_equal(start.go('down').go('up'), start)
+
